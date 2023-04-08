040d9e76ccc73edf5ef1d2b71b6db15515bbfd1a
Aman Barar <barar.amanjeet.aman@gmail.com>
2023-04-08 20:16:17 +0530

    add: Add User button

--- frontend/src/components/AddUsers.js
+++ frontend/src/components/AddUsers.js
@@ -1,8 +1,34 @@
+import { Button, FormControl, FormGroup, Input, InputLabel, TextField, Typography } from "@mui/material";
+import styled from "@emotion/styled";
+
+const Container = styled(FormGroup)`
+    width: 50%;
+    margin: 5% auto 0 auto;
+    & > div {
+        margin-top: 20px
+    }
+`
+
 const AddUser = () => {
     return(
-            <p>
-                Hello from the AddUser side
-            </p>
+            <Container>
+                <Typography variant="h6">Add User(s)</Typography>
+                    <FormControl>
+                        <InputLabel>Name</InputLabel>
+                        <Input />
+                    </FormControl>
+                    <FormControl>
+                        <InputLabel>Email</InputLabel>
+                        <Input />
+                    </FormControl>
+                    <FormControl>
+                        <InputLabel>Phone</InputLabel>
+                        <Input />
+                    </FormControl>
+                    <FormControl>
+                        <Button variant="contained">Add User</Button>
+                    </FormControl>
+            </Container>
     );
 }
 
