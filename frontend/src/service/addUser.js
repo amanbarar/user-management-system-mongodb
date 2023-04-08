10bd2d6263fca8933e783a5c1767ef4aafb3af13
Aman Barar <barar.amanjeet.aman@gmail.com>
2023-04-08 23:36:15 +0530

    add: addUser API complete

--- frontend/src/service/addUser.js
+++ frontend/src/service/addUser.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
+const URL = '';
 
-export const addUser = () => {
+export const addUser = async (data) => {
     try{
-        
+        return await axios.post(`${URL}/add`, data);
     } catch(err) {
         console.log("Error while connecting addUser", err);
     }
