6432c581e2c2f284adacfd24383010d8e3bc808e
Aman Barar <barar.amanjeet.aman@gmail.com>
2023-04-09 12:23:36 +0530

    add: PORT

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,8 @@
 import express from 'express';
 
 
+const app = express();
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
--- frontend/src/service/addUser.js
+++ frontend/src/service/addUser.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = '';
+const URL = 'http://localhost:5000';
 
 export const addUser = async (data) => {
     try{
