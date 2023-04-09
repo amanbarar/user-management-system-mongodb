9d0a0f37baa9b6089ebe7d6ed408bbbfbfbb92bd
Aman Barar <barar.amanjeet.aman@gmail.com>
2023-04-09 14:39:54 +0530

    add: body-parser

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 import Connection from './database/db.js';
 import Routes from './routes/route.js'; 
 
 const app = express();
 
+app.use(bodyParser.json({ extended:true }));
+app.use(bodyParser.urlencoded({ extended:true}));
 app.use(cors());
 app.use('/', Routes);
 
