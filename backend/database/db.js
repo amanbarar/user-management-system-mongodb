8e85a086ba4c0cb39a75e3e344a8f6ae724a8f61
Aman Barar <barar.amanjeet.aman@gmail.com>
2023-04-09 14:07:56 +0530

    add: database connection

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,6 +1,14 @@
+import mongoose from "mongoose";
 
 
-
-const Connection = () => {
-
+const Connection = async () => {
+    const URL = 'mongodb://127.0.0.1:27017/User-Management-System';
+    try{
+        await mongoose.connect(URL)
+        console.log("Database connected successfully");
+    } catch(err){
+        console.log("Error while connecting to database ",err);
+    }
 }
+
+export default Connection;
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
-
+import Connection from './database/db.js';
 
 const app = express();
 
 const PORT = 5000;
+
+Connection();
 
 app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
